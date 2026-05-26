7bca00bd509d2357c049fa7ebba58df18c08709a
JarodLge <contact.jarodlge@gmail.com>
2026-05-26 10:05:34 +0200

    feat: Implement SplashScreen widget and update main app structure

---
+++ haven_app/lib/pages/splashscreen/splashscreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          'Haven',
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
