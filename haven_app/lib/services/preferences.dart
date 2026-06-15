6415340d797bf60be35319e6fea00ed9c2cf011d
Alistair31 <gabikill@hotmail.fr>
2026-06-15 15:21:58 +0200

    feat: add PreferencesService for managing onboarding state and token storage

---
+++ haven_app/lib/services/preferences.dart
@@ -0,0 +1,29 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+class PreferencesService {
+
+  Future<void> setOnboardingSeen() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.setBool('hasSeenOnboarding', true);
+  }
+
+  Future<bool> hasSeenOnboarding() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    return prefs.getBool('hasSeenOnboarding') ?? false;
+  }
+
+  Future<void> saveToken(String token) async {
+    final savTok = await SharedPreferences.getInstance();
+
+    savTok.setString('token', token);
+  }
+
+  Future<String?> getToken() async {
+    final savTok = await SharedPreferences.getInstance();
+
+    return savTok.getString('token');
+  }
+}
